fc4bbc19d23248dce6baffc3e97d8cb3dff3ff61
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 16:07:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array){
   var arr = [];
   for (let i = 0; i < 25; i++) {
   //if (i === 1){
-    arr[i] = `"I am ${i+1} strange loop${i === 0 ? '' : 's'}."`
+    arr[i] = `I am ${i+1} strange loop${i === 0 ? '' : 's'}.`
   //}else{
   //  arr[i] = `I am ${i} strange loops.`
   //  }
